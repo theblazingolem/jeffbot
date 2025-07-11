787daf29a218c80c5c5e98d8203990e527b72f1e
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-07-11 13:50:51 +0530

    added keep alive in indexjs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 // Require the necessary discord.js classes
 const fs = require("node:fs");
+const keep_alive = require("./keep_alive.js");
 const path = require("node:path");
 const {
     Client,
--- keep_alive.js
+++ keep_alive.js
@@ -3,6 +3,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OK');
-}).listen(PORT, () => {
-    console.log(`Keepalive server running on port ${PORT}`);
+}).listen(8080, () => {
+    console.log(`Keepalive server running on port 8080`);
 });
